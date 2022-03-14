e4e0ba3d22769300287b996cbd82a2bbd0f6f63b
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-14 21:25:17 +0100

    chore: initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+
+import "@fontsource/raleway"
+import "@fontsource/montserrat"
+
+import createEmotionCache from '../utility/createEmotionCache';
+import lightThemeOptions from '../styles/theme/lightThemeOptions';
+import '../styles/globals.css';
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
