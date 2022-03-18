2452ddc82a784f169829adfdb8a7df42ec2a9a57
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-18 21:58:50 +0400

    ci: configure plop (#19)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const StyledBackgroundWrapper = styled.main`
-  background: #fff;
+  background: #111111;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -13,9 +13,9 @@ const StyledBackgroundWrapper = styled.main`
 `;
 
 const StyledHeading = styled.h1`
-  font-size: 48px;
-  color: #000;
-  font-weight: 300;
+  font-size: 64px;
+  color: #e9e9e9;
+  font-weight: 600;
   margin: 0;
   padding: 0;
   font-family: 'Montserrat', sans-serif;
