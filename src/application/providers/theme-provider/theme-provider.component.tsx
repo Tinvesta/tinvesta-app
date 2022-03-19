ef2a085e9c022893607686626fa47f2786f13b5c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-19 23:00:40 +0100

    fix: tests

--- src/application/providers/theme-provider/theme-provider.component.tsx
+++ src/application/providers/theme-provider/theme-provider.component.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider as StyledThemeProvider } from '@emotion/react';
 
-import { GlobalStyles, defaultTheme } from '@infrastructure';
+import { defaultTheme } from '@infrastructure';
+import { GlobalStyles, ResetStyles } from '@infrastructure/styles/components';
 
 import { IThemeProviderProps } from './theme-provider.types';
 
 export const ThemeProvider = ({ children }: IThemeProviderProps): JSX.Element => (
   <StyledThemeProvider theme={defaultTheme}>
+    <ResetStyles />
     <GlobalStyles />
     {children}
   </StyledThemeProvider>
--- src/infrastructure/styles/components/global-styles/global-styles.component.tsx
+++ src/infrastructure/styles/components/global-styles/global-styles.component.tsx
@@ -1,48 +1,26 @@
 import { Global, css, useTheme } from '@emotion/react';
 import '@fontsource/montserrat';
 import '@fontsource/raleway';
-import { normalize } from 'polished';
 
 export const GlobalStyles = (): JSX.Element => {
   const theme = useTheme();
 
   return (
-    <Global
-      styles={css`
-        ${normalize()}
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            color: ${theme.color.brand.secondary};
+            background-color: ${theme.color.brand.primary};
+            font-size: ${theme.typography.size.fixed.small};
+          }
 
-        html, body, div, span, applet, object, iframe,
-        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-        a, abbr, acronym, address, big, cite, code,
-        del, dfn, em, img, ins, kbd, q, s, samp,
-        small, strike, strong, sub, sup, tt, var,
-        b, u, i, center,
-        dl, dt, dd, ol, ul, li,
-        fieldset, form, label, legend,
-        table, caption, tbody, tfoot, thead, tr, th, td,
-        article, aside, canvas, details, embed,
-        figure, figcaption, footer, header, hgroup,
-        menu, nav, output, ruby, section, summary,
-        time, mark, audio, video, input {
-          border: 0;
-          margin: 0;
-          padding: 0;
-          font-size: 100%;
-          vertical-align: baseline;
-          font-family: ${theme.typography.font.montserrat};
-        }
-
-        html,
-        body {
-          color: ${theme.color.brand.secondary};
-          background-color: ${theme.color.brand.primary};
-          font-size: ${theme.typography.size.fixed.small};
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}
-    />
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      />
+    </>
   );
 };
