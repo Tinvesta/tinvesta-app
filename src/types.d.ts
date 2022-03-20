887c25b4c6fa315beee62609af227d809bc1a9f2
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-20 21:03:44 +0100

    fix: theme types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import '@emotion/react';
-
-import { defaultTheme } from '@infrastructure';
-
-type TTheme = typeof defaultTheme;
+import { Theme as MuiTheme } from '@mui/material';
 
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  export interface Theme extends TTheme {}
+  export interface Theme extends MuiTheme {}
 }
 
 declare namespace NodeJS {
