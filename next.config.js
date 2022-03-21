cfcafc587e119960d83e198e679bba7bb5fd2c24
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-21 21:52:09 +0400

    feat: configure security headers (#33)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,48 @@
 const withPWA = require('next-pwa');
 
+const ContentSecurityPolicy = `
+  script-src * 'unsafe-inline' 'unsafe-eval';
+  style-src * 'unsafe-inline' blob:;
+  img-src * data: blob:;
+  media-src * data:
+`;
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=15552000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer-when-downgrade',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
@@ -7,5 +50,14 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NEXT_PUBLIC_APP_ENV === 'local',
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
 });
