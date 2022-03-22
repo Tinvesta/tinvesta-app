5397911c0641e5932c8164d33d2b18a5d9dde200
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-22 22:02:01 +0100

    chore: update

--- src/application/pages/home/home.page.tsx
+++ src/application/pages/home/home.page.tsx
@@ -4,37 +4,29 @@ import Image from 'next/image';
 
 import { CenterBlockLayout } from '@ui';
 
-import { useDeviceDetect } from '@utils';
-
 import S from './home.styles';
 
-export const Home = (): JSX.Element => {
-  const { deviceData } = useDeviceDetect();
-
-  console.log(deviceData);
-
-  return (
-    <CenterBlockLayout>
-      <Head>
-        <title>Tinvesta</title>
-        <meta content="Tinvesta app" name="description" />
-      </Head>
-      <Image alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
-      {process.env.NEXT_PUBLIC_ENABLE_HOME_PAGE === 'true' && (
-        <>
-          <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="h1">
-            SWIPE &amp; MATCH
+export const Home = (): JSX.Element => (
+  <CenterBlockLayout>
+    <Head>
+      <title>Tinvesta</title>
+      <meta content="Tinvesta app" name="description" />
+    </Head>
+    <Image alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
+    {process.env.NEXT_PUBLIC_ENABLE_HOME_PAGE === 'true' && (
+      <>
+        <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="h1">
+          SWIPE &amp; MATCH
+        </Typography>
+        <S.StyledSubHeaderWrapper>
+          <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="body1">
+            matchmaking app for start-ups and investors all over the world
           </Typography>
-          <S.StyledSubHeaderWrapper>
-            <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="body1">
-              matchmaking app for start-ups and investors all over the world
-            </Typography>
-            <Button size="large" variant="outlined">
-              Create an account
-            </Button>
-          </S.StyledSubHeaderWrapper>
-        </>
-      )}
-    </CenterBlockLayout>
-  );
-};
+          <Button size="large" variant="outlined">
+            Create an account
+          </Button>
+        </S.StyledSubHeaderWrapper>
+      </>
+    )}
+  </CenterBlockLayout>
+);
