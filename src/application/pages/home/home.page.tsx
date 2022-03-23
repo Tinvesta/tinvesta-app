071d15c87730566161f9052116f2fb0f1868d3f2
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-23 21:40:34 +0400

    feat: create use modal hook (#44)

--- src/application/pages/home/home.page.tsx
+++ src/application/pages/home/home.page.tsx
@@ -2,31 +2,38 @@ import { Button, Typography } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import { CenterBlockLayout } from '@ui';
+import { CenterBlockLayout, useModal } from '@ui';
 
 import S from './home.styles';
 
-export const Home = (): JSX.Element => (
-  <CenterBlockLayout>
-    <Head>
-      <title>Tinvesta</title>
-      <meta content="Tinvesta app" name="description" />
-    </Head>
-    <Image alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
-    {process.env.NEXT_PUBLIC_ENABLE_HOME_PAGE === 'true' && (
-      <>
-        <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="h1">
-          SWIPE &amp; MATCH
-        </Typography>
-        <S.StyledSubHeaderWrapper>
-          <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="body1">
-            matchmaking app for start-ups and investors all over the world
+export const Home = (): JSX.Element => {
+  const { Modal, show } = useModal();
+
+  return (
+    <CenterBlockLayout>
+      <Head>
+        <title>Tinvesta</title>
+        <meta content="Tinvesta app" name="description" />
+      </Head>
+      <Modal>
+        <h1>Modal</h1>
+      </Modal>
+      <Image alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
+      {process.env.NEXT_PUBLIC_ENABLE_HOME_PAGE === 'true' && (
+        <>
+          <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="h1">
+            SWIPE &amp; MATCH
           </Typography>
-          <Button size="large" variant="outlined">
-            Create an account
-          </Button>
-        </S.StyledSubHeaderWrapper>
-      </>
-    )}
-  </CenterBlockLayout>
-);
+          <S.StyledSubHeaderWrapper>
+            <Typography fontWeight={700} sx={{ zIndex: 1 }} variant="body1">
+              matchmaking app for start-ups and investors all over the world
+            </Typography>
+            <Button size="large" variant="outlined" onClick={show}>
+              Create an account
+            </Button>
+          </S.StyledSubHeaderWrapper>
+        </>
+      )}
+    </CenterBlockLayout>
+  );
+};
