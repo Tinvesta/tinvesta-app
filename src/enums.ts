a2c6b6f0814d667c6fca67612050a4a681b04215
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-23 23:09:32 +0100

    chore: create enums to define profile and investor type

--- src/enums.ts
+++ src/enums.ts
@@ -1,3 +1,14 @@
 export enum ELocale {
   EN = 'en',
 }
+
+export enum EClientType {
+  INVESTOR = 'investor',
+  STARTUP = 'startup',
+}
+
+export enum EInvestorProfileType {
+  CORPORATE = 'corporate',
+  INSTITUTIONAL = 'institutional',
+  PRIVATE = 'private',
+}
