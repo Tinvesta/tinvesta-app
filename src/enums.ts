61857864ff8614d2a036b5da06c5da3c0524f559
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-25 01:17:54 +0400

    feat: create industrial sector enum (#51)

--- src/enums.ts
+++ src/enums.ts
@@ -25,6 +25,25 @@ export enum EStartupSector {
   SOFTWARE = 'software',
 }
 
+export enum EIndustrialSector {
+  EDUCATION = 'education',
+  ELECTRONICS_AND_AUTOMATION = 'electronics-and-automation',
+  ENERGY = 'energy',
+  ENVIRONMENT = 'environment',
+  INFRASTRUCTURE = 'infrastructure',
+  JOBS_RECRUITMENT = 'jobs-recruitment',
+  LIVING_AND_FAMILY = 'living-and-family',
+  MATERIALS = 'materials',
+  MEDIA_AND_ENTERTAINMENT = 'media-and-entertainment',
+  REAL_ESTATE = 'real-estate',
+  SECURITY = 'security',
+  SOCIAL = 'social',
+  SPORTS_AND_FASHION = 'sports-and-fashion',
+  TELECOM = 'telecom',
+  TRAVEL_AND_HOSPITALITY = 'travel-and-hospitality',
+  WELLNESS_AND_BEAUTY = 'wellness-and-beauty',
+}
+
 export enum EInvestorProfileType {
   CORPORATE = 'corporate',
   INSTITUTIONAL = 'institutional',
