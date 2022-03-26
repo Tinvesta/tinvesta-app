879c6ea5dc3fe3a9f982750ba152f353118cd3a6
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-26 16:58:54 +0100

    chore: update

--- src/domain/registration/registration.component.tsx
+++ src/domain/registration/registration.component.tsx
@@ -1,18 +1,3 @@
-import { useState } from 'react';
-
-import { LocationAutocomplete } from './atoms';
 import S from './registration.styles';
 
-export const Registration = (): JSX.Element => {
-  const [location, setLocation] = useState('');
-
-  return (
-    <S.StyledWrapper>
-      Registration
-      <LocationAutocomplete
-        inputValue={location}
-        onInputChange={(_, newValue) => setLocation(newValue)}
-      />
-    </S.StyledWrapper>
-  );
-};
+export const Registration = (): JSX.Element => <S.StyledWrapper>Registration</S.StyledWrapper>;
