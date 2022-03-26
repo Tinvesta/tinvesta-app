128833287c7f0032dc205e4e88c28c581f760651
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-26 16:28:43 +0400

    feat: particles as global background (#59)

--- src/application/pages/release-date/release-date.page.tsx
+++ src/application/pages/release-date/release-date.page.tsx
@@ -2,7 +2,6 @@ import { Typography } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import { ParticlesBackground } from '../home/atoms';
 import S from './release-date.styles';
 
 export const ReleaseDatePage = (): JSX.Element => (
@@ -11,7 +10,6 @@ export const ReleaseDatePage = (): JSX.Element => (
       <title>Tinvesta</title>
       <meta content="Tinvesta app" name="description" />
     </Head>
-    <ParticlesBackground />
     <Image alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
     <Typography variant="h3">26.05.2022</Typography>
   </S.StyledWrapper>
