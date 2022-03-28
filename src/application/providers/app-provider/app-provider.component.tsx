d4158664d6b3458a50f99f0402edb9eec54ef2c0
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-28 18:45:27 +0200

    chore: update roadmap

--- src/application/providers/app-provider/app-provider.component.tsx
+++ src/application/providers/app-provider/app-provider.component.tsx
@@ -8,20 +8,16 @@ import {
 } from '..';
 import { IAppProviderProps } from './app-provider.types';
 
-export const AppProvider = ({ children, emotionCache }: IAppProviderProps): JSX.Element => {
-  console.log(emotionCache, 'emotionCache');
-
-  return (
-    <ThemeProvider>
-      <CacheProvider emotionCache={emotionCache}>
-        <ToastProvider>
-          <LocaleProvider>
-            <UserProvider>
-              <QueryClientProvider>{children}</QueryClientProvider>
-            </UserProvider>
-          </LocaleProvider>
-        </ToastProvider>
-      </CacheProvider>
-    </ThemeProvider>
-  );
-};
+export const AppProvider = ({ children, emotionCache }: IAppProviderProps): JSX.Element => (
+  <ThemeProvider>
+    <CacheProvider emotionCache={emotionCache}>
+      <ToastProvider>
+        <LocaleProvider>
+          <UserProvider>
+            <QueryClientProvider>{children}</QueryClientProvider>
+          </UserProvider>
+        </LocaleProvider>
+      </ToastProvider>
+    </CacheProvider>
+  </ThemeProvider>
+);
