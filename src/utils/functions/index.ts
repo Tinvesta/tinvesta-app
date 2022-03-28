9f0a48d3112d151fd51d2ceb6bb7dc147eb1fefd
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-28 16:54:14 +0200

    feat: create try catch wrapper function

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -2,6 +2,7 @@ export * from './camel-case/camel-case.function';
 export * from './get-window-size/get-window-size.function';
 export * from './get-window-width/get-window-width.function';
 export * from './get-window-height/get-window-height.function';
+export * from './try-catch-wrapper/try-catch-wrapper.function';
 export * from './create-emotion-cache/create-emotion-cache.function';
 export * from './object-to-query-string/object-to-query-string.function';
 export * from './get-available-languages/get-available-languages.function';
