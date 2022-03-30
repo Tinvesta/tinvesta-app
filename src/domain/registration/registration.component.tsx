25d676c826b19a41ae9007d63634d871da849b12
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-30 19:39:00 +0400

    feat: create select with controller (#71)

--- src/domain/registration/registration.component.tsx
+++ src/domain/registration/registration.component.tsx
@@ -1,3 +1,81 @@
+import { Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+import { SelectWithController, TextFieldWithController } from '@ui';
+
 import S from './registration.styles';
 
-export const Registration = (): JSX.Element => <S.StyledWrapper>Registration</S.StyledWrapper>;
+export const Registration = (): JSX.Element => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: { name: '', selectedValue: '', description: '' },
+  });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  if (process.env.NEXT_PUBLIC_ENABLE_HOME_PAGE !== 'true') {
+    return <S.StyledWrapper>Registration</S.StyledWrapper>;
+  }
+
+  return (
+    <S.StyledWrapper>
+      Registration
+      <form
+        style={{ padding: '20px', display: 'flex', flexDirection: 'column', maxWidth: '300px' }}
+        onSubmit={onSubmit}
+      >
+        <TextFieldWithController
+          controllerProps={{
+            control,
+            name: 'name',
+            rules: {
+              required: true,
+            },
+          }}
+          inputProps={{
+            autoComplete: 'general.phoneNumber',
+            label: 'Name',
+          }}
+        />
+        <SelectWithController
+          controllerProps={{
+            control,
+            name: 'selectedValue',
+            rules: {
+              required: true,
+            },
+          }}
+          selectProps={{
+            options: [
+              {
+                label: 'Test 1',
+                value: 'test-1',
+              },
+              {
+                label: 'Test 2',
+                value: 'test-2',
+              },
+            ],
+            labelId: 'customers-dropdown-label',
+            label: 'Test Label',
+          }}
+        />
+        <TextFieldWithController
+          controllerProps={{
+            control,
+            name: 'description',
+            rules: {
+              required: true,
+            },
+          }}
+          inputProps={{
+            autoComplete: 'general.phoneNumber',
+            label: 'Description',
+          }}
+        />
+        <Button type="submit" variant="outlined">
+          Submit
+        </Button>
+      </form>
+    </S.StyledWrapper>
+  );
+};
