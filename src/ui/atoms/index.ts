6560b4b3743d26be08170c7e5dd9cbd1a59f47ee
playerony <pawel.wojtasinski.1995@gmail.com>
2022-03-30 15:19:16 +0200

    feat: create select component

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,4 +1,6 @@
+export * from './select/select.component';
 export * from './password-field/password-field.component';
 export * from './upload-image-with-preview/upload-image-with-preview.component';
 
+export * from './select/select.types';
 export * from './password-field/password-field.types';
