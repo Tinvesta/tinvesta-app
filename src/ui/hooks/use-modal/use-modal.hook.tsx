87239d2ba446a5201e761b06e6c476f4570dfb3c
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-03-31 22:14:57 +0400

    feat: add ability to remove uploaded image (#76)

--- src/ui/hooks/use-modal/use-modal.hook.tsx
+++ src/ui/hooks/use-modal/use-modal.hook.tsx
@@ -1,15 +1,22 @@
-import { ModalProps, Modal as MuiModal } from '@mui/material';
+import { Modal as MuiModal } from '@mui/material';
 import { useCallback, useState } from 'react';
 
 import S from './use-modal.styles';
+import { IModalProps, IUseModalAttributes } from './use-modal.types';
 
-export const useModal = (defaultIsOpenState: boolean = false) => {
+export const useModal = ({
+  defaultIsOpenState = false,
+  withCloseIcon = true,
+}: IUseModalAttributes = {}) => {
   const [isOpen, setIsOpen] = useState(defaultIsOpenState);
   const show = () => setIsOpen(true);
   const hide = () => setIsOpen(false);
 
-  const Modal = ({ children, onClose, ...restProps }: Omit<ModalProps, 'open'>): JSX.Element => {
-    const handleClose = (event: {}, reason: 'backdropClick' | 'escapeKeyDown') => {
+  const Modal = ({ children, onClose, ...restProps }: IModalProps): JSX.Element => {
+    const handleClose = (
+      event: {},
+      reason: 'backdropClick' | 'escapeKeyDown' | 'closeIconClick',
+    ) => {
       hide();
 
       if (onClose) {
@@ -17,11 +24,13 @@ export const useModal = (defaultIsOpenState: boolean = false) => {
       }
     };
 
+    const onCloseIconClick = () => handleClose({}, 'closeIconClick');
+
     return (
       <MuiModal open={isOpen} onClose={handleClose} {...restProps}>
         <S.StyledContentWrapper>
           <>
-            <S.StyledCloseIcon onClick={hide} />
+            {withCloseIcon && <S.StyledCloseIcon onClick={onCloseIconClick} />}
             {children}
           </>
         </S.StyledContentWrapper>
