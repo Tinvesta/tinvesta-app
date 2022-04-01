c38b81b65438a3ccd31f69a010ffd74cd352aac3
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-01 23:36:30 +0200

    chore: add error handling for image compression

--- src/ui/atoms/upload-image-with-preview/upload-image-with-preview.defaults.ts
+++ src/ui/atoms/upload-image-with-preview/upload-image-with-preview.defaults.ts
@@ -1,5 +1,6 @@
 export const translationStrings = [
   'component.upload.image.with.preview.modal.title',
+  'component.upload.image.with.preview.error.compression',
   'component.upload.image.with.preview.modal.button.text',
   'component.upload.image.with.preview.image.scaled.image.alt',
   'component.upload.image.with.preview.image.upload.button.text',
