00e3a613d1333e47be6437bd7735112ce82d796b
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-11 21:29:17 +0200

    feat: send request to create profile endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,7 @@ export const SMALL_SCREEN_BREAKPOINT = 992;
 export const MOBILE_SCREEN_BREAKPOINT = 768;
 export const XMOBILE_SCREEN_BREAKPOINT = 480;
 
+export const STARTUP_CLIENT_TYPE_ID = 1;
+
 export const DEFAULT_LOCALE: ELocale =
   (process.env.NEXT_PUBLIC_DEFAULT_LOCALE as ELocale) ?? ELocale.EN;
