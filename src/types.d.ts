3258c8a5b2292e95c5b664e97fb04e26718d2a8c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-17 22:42:52 +0200

    chore: update

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,17 +3,7 @@
 import '@emotion/react';
 import { Theme as MuiTheme } from '@mui/material';
 
-// import { ReactNode } from 'react';
-// import { QueryClientProviderProps as IQueryClientProviderProps } from 'react-query';
-
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   export interface Theme extends MuiTheme {}
 }
-
-// declare module 'react-query' {
-//   // eslint-disable-next-line @typescript-eslint/naming-convention
-//   export interface QueryClientProviderProps extends IQueryClientProviderProps {
-//     children: ReactNode;
-//   }
-// }
