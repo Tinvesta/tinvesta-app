21c44bdf29ba579f09620ac9075a3a4f5ffd1bbf
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-17 22:04:16 +0200

    feat: create matches page

---
+++ pages/dashboard/matches.tsx
@@ -0,0 +1,6 @@
+import { dashboardMatchesPage } from '@application';
+
+const { getServerSideProps, MatchesPage } = dashboardMatchesPage;
+
+// eslint-disable-next-line no-restricted-exports
+export { MatchesPage as default, getServerSideProps };
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,7 @@
 export * from './home/home.component';
 export * from './likes/likes.component';
 export * from './terms/terms.component';
+export * from './matches/matches.component';
 export * from './pricing/pricing.component';
 export * from './discover/discover.component';
 export * from './onboarding/onboarding.component';
