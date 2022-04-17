db8ebb9f7eec313a8431449fe1166bd4363ec1ad
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-17 19:51:30 +0200

    feat: create use sessino storage hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -4,4 +4,5 @@ export * from './use-set-state/use-set-state.hook';
 export * from './use-translation/use-translation.hook';
 export * from './use-device-detect/use-device-detect.hook';
 export * from './use-local-storage/use-local-storage.hook';
+export * from './use-session-storage/use-session-storage.hook';
 export * from './use-why-did-you-update/use-why-did-you-update.hook';
