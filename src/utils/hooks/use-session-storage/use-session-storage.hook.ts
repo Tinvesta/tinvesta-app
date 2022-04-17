5149e3c90ab7de7e6ab476df94fba8d38f0323e6
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-04-17 21:56:57 +0400

    feat: create use sessino storage hook (#134)

---
+++ src/utils/hooks/use-session-storage/use-session-storage.hook.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { tryCatchWrapper, tryParseJson } from '@utils';
+
+export const useSessionStorage = <T>(
+  key: string,
+  initialValue?: T,
+): [T | undefined, (value: T) => void] => {
+  const [storedValue, setStoredValue] = useState<T | undefined>(() => {
+    const item = tryCatchWrapper(() => sessionStorage.getItem(key), console.log);
+
+    return tryParseJson<T>(item ?? '') ?? initialValue;
+  });
+
+  const setValue = (value: T) => {
+    tryCatchWrapper(
+      () => {
+        sessionStorage.setItem(key, JSON.stringify(value));
+
+        setStoredValue(value);
+      },
+      (_error) => console.error('[use-session-storage]', _error),
+    );
+  };
+
+  return [storedValue, setValue];
+};
---
+++ src/utils/hooks/use-session-storage/use-session-storage.spec.ts
@@ -0,0 +1,7 @@
+import { functionImportTest } from '@utils';
+
+import { useSessionStorage } from './use-session-storage.hook';
+
+describe('useSessionStorage hook', () => {
+  functionImportTest(useSessionStorage);
+});
