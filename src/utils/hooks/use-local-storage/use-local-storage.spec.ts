62df67a81e1013f4382e0eecb64498e1def62287
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-04-17 21:48:25 +0400

    feat: create use local storage hook (#133)

---
+++ src/utils/hooks/use-local-storage/use-local-storage.spec.ts
@@ -0,0 +1,7 @@
+import { functionImportTest } from '@utils';
+
+import { useLocalStorage } from './use-local-storage.hook';
+
+describe('useLocalStorage hook', () => {
+  functionImportTest(useLocalStorage);
+});
