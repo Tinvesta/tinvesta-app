bc3de8426b9879888ee0e371363d54a6fb1ed0ec
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-04-18 20:06:44 +0400

    feat: create side menu user avatar (#142)

--- src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.layout.tsx
+++ src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.layout.tsx
@@ -1,9 +1,37 @@
+import { Avatar, Typography } from '@mui/material';
+import Image from 'next/image';
+
+import { useUser } from '@utils';
+
 import S from './desktop-dashboard.styles';
 import { IDesktopDashboardLayoutProps } from './desktop-dashboard.types';
 
-export const DesktopDashboardLayout = ({ children }: IDesktopDashboardLayoutProps): JSX.Element => (
-  <S.StyledWrapper>
-    <S.StyledAsideWrapper>Aside</S.StyledAsideWrapper>
-    <S.StyledContentWrapper>{children}</S.StyledContentWrapper>
-  </S.StyledWrapper>
-);
+export const DesktopDashboardLayout = ({ children }: IDesktopDashboardLayoutProps): JSX.Element => {
+  const { user } = useUser();
+
+  return (
+    <S.StyledWrapper>
+      <S.StyledAsideWrapper>
+        <S.StyledLogoWrapper>
+          <Image
+            priority
+            alt="Tinvesta"
+            height={28}
+            objectFit="fill"
+            src="/images/brandmark-full-logo-transparent-white.png"
+            width={200}
+          />
+        </S.StyledLogoWrapper>
+        <S.StyledUserInfoWrapper>
+          <Avatar src={user?.user_metadata?.avatar_url} sx={{ width: 56, height: 56 }} />
+          <S.StyledUserInfoDetails>
+            <Typography>
+              {user?.first_name} {user?.last_name}
+            </Typography>
+          </S.StyledUserInfoDetails>
+        </S.StyledUserInfoWrapper>
+      </S.StyledAsideWrapper>
+      <S.StyledContentWrapper>{children}</S.StyledContentWrapper>
+    </S.StyledWrapper>
+  );
+};
--- src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.styles.ts
+++ src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.styles.ts
@@ -1,21 +1,42 @@
 import styled from '@emotion/styled';
 
 const StyledWrapper = styled.div`
-  display: flex;
   height: 100%;
+  display: flex;
 `;
 
 const StyledAsideWrapper = styled.aside`
   width: 350px;
+  padding: ${({ theme }) => theme.spacing(3)};
   background-color: ${({ theme }) => theme.palette.grey[800]};
 `;
+
+const StyledLogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spacing(3)} 0 ${({ theme }) => theme.spacing(4)} 0;
+`;
+
+const StyledUserInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing(3)};
+  padding: ${({ theme }) => theme.spacing(3)};
+  background-color: ${({ theme }) => theme.palette.grey[700]};
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+`;
+
+const StyledUserInfoDetails = styled.div``;
 
 const StyledContentWrapper = styled.div``;
 
 const S = {
   StyledWrapper,
+  StyledLogoWrapper,
   StyledAsideWrapper,
   StyledContentWrapper,
+  StyledUserInfoDetails,
+  StyledUserInfoWrapper,
 };
 
 export default S;
