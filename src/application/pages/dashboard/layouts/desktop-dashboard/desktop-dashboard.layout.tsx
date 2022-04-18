6f74d8b9da82257614ab45d8f8ca2b927e2a7676
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-04-18 21:46:19 +0400

    feat: add simple aside menu (#143)

--- src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.layout.tsx
+++ src/application/pages/dashboard/layouts/desktop-dashboard/desktop-dashboard.layout.tsx
@@ -1,7 +1,18 @@
+import {
+  CardMembership as CardMembershipIcon,
+  Gavel as GavelIcon,
+  JoinInner as JoinInnerIcon,
+  Policy as PolicyIcon,
+  StarBorder as StarBorderIcon,
+  Style as StyleIcon,
+} from '@mui/icons-material';
 import { Avatar, Typography } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { useUser } from '@utils';
+
+import { ERoutes } from '@enums';
 
 import S from './desktop-dashboard.styles';
 import { IDesktopDashboardLayoutProps } from './desktop-dashboard.types';
@@ -30,6 +41,58 @@ export const DesktopDashboardLayout = ({ children }: IDesktopDashboardLayoutProp
             </Typography>
           </S.StyledUserInfoDetails>
         </S.StyledUserInfoWrapper>
+        <S.StyledMenu>
+          <Link passHref href={ERoutes.DASHBOARD_DISCOVER}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <StyleIcon />
+              </S.StyledMenuItemIcon>
+              Discover
+            </S.StyledMenuItem>
+          </Link>
+          <Link passHref href={ERoutes.DASHBOARD_MATCHES}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <JoinInnerIcon />
+              </S.StyledMenuItemIcon>
+              Matches
+            </S.StyledMenuItem>
+          </Link>
+          <Link passHref href={ERoutes.DASHBOARD_LIKES}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <StarBorderIcon />
+              </S.StyledMenuItemIcon>
+              Likes
+            </S.StyledMenuItem>
+          </Link>
+          <Link passHref href={ERoutes.DASHBOARD_SUBSCRIPTION}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <CardMembershipIcon />
+              </S.StyledMenuItemIcon>
+              Subscription
+            </S.StyledMenuItem>
+          </Link>
+        </S.StyledMenu>
+        <S.StyledMenu>
+          <Link passHref href={ERoutes.DASHBOARD_DISCOVER}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <PolicyIcon />
+              </S.StyledMenuItemIcon>
+              Privacy and Policy
+            </S.StyledMenuItem>
+          </Link>
+          <Link passHref href={ERoutes.DASHBOARD_MATCHES}>
+            <S.StyledMenuItem>
+              <S.StyledMenuItemIcon>
+                <GavelIcon />
+              </S.StyledMenuItemIcon>
+              Terms and Conditions
+            </S.StyledMenuItem>
+          </Link>
+        </S.StyledMenu>
       </S.StyledAsideWrapper>
       <S.StyledContentWrapper>{children}</S.StyledContentWrapper>
     </S.StyledWrapper>
