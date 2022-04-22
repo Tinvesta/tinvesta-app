3a2602a193a3eb2db70d3fde2688bc921f4eee2c
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-04-22 22:53:33 +0400

    feat: create profile details function (#158)

--- src/domain/discover/api/get-profile-details/get-profile-details.action.ts
+++ src/domain/discover/api/get-profile-details/get-profile-details.action.ts
@@ -5,4 +5,4 @@ import { nextAxiosInstance } from '@infrastructure';
 import { EApiEndpoint } from '@enums';
 
 export const getProfileDetailsAction = async (profileId: string) =>
-  nextAxiosInstance.get(`${EApiEndpoint.GET_PROFILE_DETAILS}${objectToQueryString({ profileId })}`);
+  nextAxiosInstance.get(`${EApiEndpoint.PROFILE_DETAILS}${objectToQueryString({ profileId })}`);
