c96abd2d7eede1576247279ab8c94450afbcf88a
playerony <pawel.wojtasinski.1995@gmail.com>
2022-04-30 20:44:42 +0200

    feat: display loader at the top of the page

--- src/application/app/app.component.tsx
+++ src/application/app/app.component.tsx
@@ -1,3 +1,5 @@
+import NextNProgress from 'nextjs-progressbar';
+
 import { AppProvider } from '@application';
 
 import S from './app.styles';
@@ -8,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const App = ({ Component, emotionCache, pageProps }: IAppProps) => (
   <AppProvider emotionCache={emotionCache}>
     <S.StyledWrapper>
+      <NextNProgress color="#65AFFF" stopDelayMs={200} />
       <Component {...pageProps} />
     </S.StyledWrapper>
   </AppProvider>
