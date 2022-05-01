cb475a6987554be6a3c63052f9bbe95f1138cb58
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-01 21:00:29 +0200

    feat: show label for active route

--- src/application/pages/dashboard/layouts/mobile-dashboard/mobile-dashboard.defaults.tsx
+++ src/application/pages/dashboard/layouts/mobile-dashboard/mobile-dashboard.defaults.tsx
@@ -7,21 +7,32 @@ import {
 
 import { ERoutes } from '@enums';
 
-export const bottomNavigationOptions = [
+export const translationStrings = [
+  'component.dashboard.sidemenu.option.discover',
+  'component.dashboard.sidemenu.option.matches',
+  'component.dashboard.sidemenu.option.likes',
+  'component.dashboard.sidemenu.option.settings',
+] as const;
+
+export const getBottomNavigationOptions = (translations: Record<string, string>) => [
   {
     icon: <StyleIcon />,
     route: ERoutes.DASHBOARD_DISCOVER,
+    label: translations.componentDashboardSidemenuOptionDiscover,
   },
   {
     icon: <JoinInnerIcon />,
     route: ERoutes.DASHBOARD_MATCHES,
+    label: translations.componentDashboardSidemenuOptionMatches,
   },
   {
     icon: <StarBorderIcon />,
     route: ERoutes.DASHBOARD_LIKES,
+    label: translations.componentDashboardSidemenuOptionLikes,
   },
   {
     icon: <SettingsIcon />,
     route: ERoutes.DASHBOARD_SETTINGS,
+    label: translations.componentDashboardSidemenuOptionSettings,
   },
 ];
