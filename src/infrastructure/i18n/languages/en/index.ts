0f00e98dd9a3d09991949357a76317e6967bd5e2
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-01 18:42:02 +0200

    feat: fill desktop dashboard sidemenu with translations

--- src/infrastructure/i18n/languages/en/index.ts
+++ src/infrastructure/i18n/languages/en/index.ts
@@ -1,10 +1,12 @@
 import commonTranslations from './common.translations';
+import dashboardTranslations from './dashboard';
 import locationAutocompleteTranslations from './location-autocomplete.translations';
 import onboardingTranslations from './onboarding';
 import uploadImagesTranslations from './upload-images.translations';
 
 export default {
   ...commonTranslations,
+  ...dashboardTranslations,
   ...uploadImagesTranslations,
   ...locationAutocompleteTranslations,
   ...onboardingTranslations,
