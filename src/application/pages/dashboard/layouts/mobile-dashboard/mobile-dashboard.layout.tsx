24ba03978e77c7a4f1af3f7d4b324f32546c1c1e
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-01 20:58:47 +0200

    chore: remove translations

--- src/application/pages/dashboard/layouts/mobile-dashboard/mobile-dashboard.layout.tsx
+++ src/application/pages/dashboard/layouts/mobile-dashboard/mobile-dashboard.layout.tsx
@@ -2,21 +2,16 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import { useTranslation } from '@utils';
-
 import { ERoutes } from '@enums';
 
-import { getBottomNavigationOptions, translationStrings } from './mobile-dashboard.defaults';
+import { bottomNavigationOptions } from './mobile-dashboard.defaults';
 import S from './mobile-dashboard.styles';
 import { IMobileDashboardLayoutProps } from './mobile-dashboard.types';
 
 export const MobileDashboardLayout = ({ children }: IMobileDashboardLayoutProps): JSX.Element => {
   const router = useRouter();
-  const translations = useTranslation(translationStrings);
 
   const isActiveOption = (route: ERoutes): boolean => router.pathname === route;
-
-  const bottomNavigationOptions = getBottomNavigationOptions(translations);
 
   return (
     <S.StyledWrapper>
@@ -29,11 +24,10 @@ export const MobileDashboardLayout = ({ children }: IMobileDashboardLayoutProps)
           const isActive = isActiveOption(_bottomNavigationOption.route);
 
           return (
-            <Link key={_bottomNavigationOption.label} passHref href={_bottomNavigationOption.route}>
+            <Link key={_bottomNavigationOption.route} passHref href={_bottomNavigationOption.route}>
               <S.StyledBottomNavigationAction
                 active={isActive}
                 icon={_bottomNavigationOption.icon}
-                label={_bottomNavigationOption.label}
               />
             </Link>
           );
