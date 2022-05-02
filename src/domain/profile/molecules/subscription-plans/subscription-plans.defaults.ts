9a91df00f5ad8a04c78d6690a9010ea20f9053b6
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-02 20:59:37 +0200

    feat: create subscription plans component

---
+++ src/domain/profile/molecules/subscription-plans/subscription-plans.defaults.ts
@@ -0,0 +1,14 @@
+export const translationStrings = [
+  'component.dashboard.subscription.currency',
+  'component.dashboard.subscription.header',
+  'component.dashboard.subscription.benefits',
+  'component.dashboard.subscription.benefit.one',
+  'component.dashboard.subscription.benefit.two',
+  'component.dashboard.subscription.benefit.three',
+  'component.dashboard.subscription.benefit.four',
+  'component.dashboard.subscription.monthly.header',
+  'component.dashboard.subscription.monthly.button',
+  'component.dashboard.subscription.yearly.header',
+  'component.dashboard.subscription.yearly.subheader',
+  'component.dashboard.subscription.yearly.button',
+] as const;
--- src/domain/profile/molecules/subscription-plans/subscription-plans.styles.ts
+++ src/domain/profile/molecules/subscription-plans/subscription-plans.styles.ts
@@ -1,9 +1,57 @@
 import styled from '@emotion/styled';
+import { Button, List, Paper } from '@mui/material';
 
-const StyledWrapper = styled.div``;
+import { respondToMax } from '@infrastructure';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(4)};
+  margin-top: ${({ theme }) => theme.spacing(3)};
+
+  ${({ theme }) => respondToMax.mobile`
+    flex-direction: column;
+    gap: ${theme.spacing(3)};
+  `}
+`;
+
+const StyledPaper = styled(Paper)`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spacing(4)};
+
+  ${respondToMax.small`
+    width: 350px;
+  `}
+
+  ${({ theme }) => respondToMax.xmobile`
+    width: 100%;
+    padding: ${theme.spacing(3)};
+  `}
+`;
+
+const StyledSubscriptionPaperButton = styled(Button)`
+  margin-top: ${({ theme }) => theme.spacing(4)};
+`;
+
+const StyledList = styled(List)`
+  .MuiListItemAvatar-root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    ${respondToMax.xmobile`
+      min-width: 35px;
+    `}
+  }
+`;
 
 const S = {
+  StyledList,
+  StyledPaper,
   StyledWrapper,
+  StyledSubscriptionPaperButton,
 };
 
 export default S;
