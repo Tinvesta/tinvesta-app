9c42fe47f189a5aae0dc86ecb7f696cca626d649
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-04 21:25:58 +0400

    feat: improve profile details endpoint (#227)

--- src/domain/profile/molecules/edit-profile-form/edit-profile-form.component.tsx
+++ src/domain/profile/molecules/edit-profile-form/edit-profile-form.component.tsx
@@ -6,14 +6,17 @@ import { PROFILE_DETAILS_ACTION_QUERY_KEY, profileDetailsAction } from '../../ap
 import { SectionWrapperLayout } from '../../atoms';
 import { IEditProfileFormProps } from './edit-profile-form.types';
 
-export const EditProfileForm = ({ clientTypes }: IEditProfileFormProps): JSX.Element => {
+export const EditProfileForm = ({
+  clientTypes,
+  focusMarkets,
+}: IEditProfileFormProps): JSX.Element => {
   const { user } = useUser();
   const { data: profileDetailsActionData, isLoading: isProfileDetailsActionLoading } = useQuery(
     [PROFILE_DETAILS_ACTION_QUERY_KEY, user?.id],
     profileDetailsAction(user?.id),
   );
 
-  console.log(clientTypes, profileDetailsActionData, isProfileDetailsActionLoading);
+  console.log(clientTypes, focusMarkets, profileDetailsActionData, isProfileDetailsActionLoading);
 
   return <SectionWrapperLayout title="Edit profile">EditProfileForm</SectionWrapperLayout>;
 };
