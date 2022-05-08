51d56c5870960a5568bc20ab71927b42ad5e9666
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-08 17:28:20 +0400

    refactor: remove unused hooks (#261)

--- src/utils/hooks/use-why-did-you-update/use-why-did-you-update.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@utils';
-
-import { useWhyDidYouUpdate } from './use-why-did-you-update.hook';
-
-describe('useWhyDidYouUpdate hook', () => {
-  functionImportTest(useWhyDidYouUpdate);
-});
