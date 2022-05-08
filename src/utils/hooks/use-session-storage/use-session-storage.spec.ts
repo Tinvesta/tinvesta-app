70dfa03447c506428d3709bef4cc2a703aeb3c1b
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-08 15:23:47 +0200

    refactor: remove unused hooks

--- src/utils/hooks/use-session-storage/use-session-storage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@utils';
-
-import { useSessionStorage } from './use-session-storage.hook';
-
-describe('useSessionStorage hook', () => {
-  functionImportTest(useSessionStorage);
-});
