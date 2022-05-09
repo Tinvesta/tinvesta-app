6c1da18d7888049d87757e34a01d88be5ec478eb
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-09 21:30:53 +0400

    feat: fill match modal with base data (#273)

--- src/domain/discover/molecules/match-modal-content/match-modal-content.component.tsx
+++ src/domain/discover/molecules/match-modal-content/match-modal-content.component.tsx
@@ -1,10 +1,49 @@
-import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
+import Image from 'next/image';
+
+import { Loading } from '@ui';
 
 import S from './match-modal-content.styles';
 import { IMatchModalContentProps } from './match-modal-content.types';
 
-export const MatchModalContent = ({ onClose }: IMatchModalContentProps): JSX.Element => (
-  <S.StyledWrapper>
-    <Button onClick={onClose}>Close</Button>
-  </S.StyledWrapper>
-);
+export const MatchModalContent = ({
+  likedProfileDetails,
+  loggedProfileDetails,
+  onClose,
+}: IMatchModalContentProps): JSX.Element => {
+  if (!likedProfileDetails || !loggedProfileDetails) {
+    return <Loading />;
+  }
+
+  return (
+    <S.StyledWrapper>
+      <Typography variant="h2">It&apos;s a match!</Typography>
+      <S.StyledImageContainer>
+        <S.StyledImageWrapper>
+          <Image
+            alt="Profile image"
+            height={450}
+            objectFit="cover"
+            src={loggedProfileDetails?.avatars[0]}
+            width={300}
+          />
+        </S.StyledImageWrapper>
+        <S.StyledImageWrapper>
+          <Image
+            alt="Profile image"
+            height={450}
+            objectFit="cover"
+            src={likedProfileDetails.avatars[0]}
+            width={300}
+          />
+        </S.StyledImageWrapper>
+      </S.StyledImageContainer>
+      <S.StyledButton size="large" variant="contained" onClick={onClose}>
+        Write an email
+      </S.StyledButton>
+      <S.StyledButton size="large" variant="outlined" onClick={onClose}>
+        Keep swiping
+      </S.StyledButton>
+    </S.StyledWrapper>
+  );
+};
--- src/domain/discover/molecules/match-modal-content/match-modal-content.styles.ts
+++ src/domain/discover/molecules/match-modal-content/match-modal-content.styles.ts
@@ -1,12 +1,38 @@
 import styled from '@emotion/styled';
+import { Button } from '@mui/material';
 
 const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing(4)};
   width: calc(100vw - ${({ theme }) => `${theme.spacing(3)} - ${theme.spacing(3)}`} - 2px);
   height: calc(100vh - ${({ theme }) => `${theme.spacing(3)} - ${theme.spacing(3)}`} - 2px);
 `;
 
+const StyledImageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing(4)};
+`;
+
+const StyledImageWrapper = styled.div`
+  width: fit-content;
+  justify-content: space-around;
+  border: 1px solid ${({ theme }) => theme.palette.divider};
+`;
+
+const StyledButton = styled(Button)`
+  min-width: 200px;
+`;
+
 const S = {
+  StyledButton,
   StyledWrapper,
+  StyledImageWrapper,
+  StyledImageContainer,
 };
 
 export default S;
