e1287e1b0d44d269e02947717a93420535bd1be7
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-10 22:03:55 +0200

    chore: update

--- src/domain/discover/utils/functions/transform-number-array-to-chips/transform-number-array-to-chips.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@utils';
-
-import { transformNumberArrayToChips } from './transform-number-array-to-chips.function';
-
-describe('transformNumberArrayToChips function', () => {
-  functionImportTest(transformNumberArrayToChips);
-});
