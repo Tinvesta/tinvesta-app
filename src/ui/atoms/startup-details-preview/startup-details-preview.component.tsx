75b11ad7403fda97d803359bcddaf6ce0c300a1e
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-11 22:19:11 +0400

    feat: fetch startup details (#284)

--- src/ui/atoms/startup-details-preview/startup-details-preview.component.tsx
+++
@@ -1,8 +0,0 @@
-import S from './startup-details-preview.styles';
-import { IStartupDetailsPreviewProps } from './startup-details-preview.types';
-
-export const StartupDetailsPreview = ({
-  profileDetails,
-}: IStartupDetailsPreviewProps): JSX.Element => (
-  <S.StyledWrapper>{JSON.stringify(profileDetails, null, 2)}</S.StyledWrapper>
-);
---
+++ src/ui/molecules/startup-details-preview/startup-details-preview.component.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query';
+
+import { PROFILE_DETAILS_ACTION_QUERY_KEY, profileDetailsAction } from './api';
+import S from './startup-details-preview.styles';
+import { IStartupDetailsPreviewProps } from './startup-details-preview.types';
+
+export const StartupDetailsPreview = ({
+  profileDetails,
+}: IStartupDetailsPreviewProps): JSX.Element => {
+  const { data: fetchedProfileDetails } = useQuery(
+    [PROFILE_DETAILS_ACTION_QUERY_KEY, profileDetails.id],
+    profileDetailsAction(profileDetails.id),
+  );
+
+  console.log(fetchedProfileDetails);
+
+  return <S.StyledWrapper>{JSON.stringify(profileDetails, null, 2)}</S.StyledWrapper>;
+};
