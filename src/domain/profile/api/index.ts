9441c4d13be8f48b33ff562fa480c11ec2ad7eb6
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-11 20:25:21 +0200

    refactor: profiel details action query position

--- src/domain/profile/api/index.ts
+++ src/domain/profile/api/index.ts
@@ -1,4 +1,3 @@
 export * from './subscription/subscription.action';
 export * from './stripe-portal/stripe-portal.action';
 export * from './update-profile/update-profile.action';
-export * from './profile-details/profile-details.action';
