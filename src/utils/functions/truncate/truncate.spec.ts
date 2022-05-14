31610620604d84af1076d18f3616bfd87dd3d48d
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-14 12:59:55 +0200

    test: improve test coverage of truncate function

--- src/utils/functions/truncate/truncate.spec.ts
+++ src/utils/functions/truncate/truncate.spec.ts
@@ -4,4 +4,36 @@ import { truncate } from './truncate.function';
 
 describe('truncate function', () => {
   functionImportTest(truncate);
+
+  it('should return empty string when provided parameter is not a string value', () => {
+    // @ts-expect-error
+    expect(truncate()).toEqual('');
+    // @ts-expect-error
+    expect(truncate({})).toEqual('');
+    // @ts-expect-error
+    expect(truncate(null)).toEqual('');
+    // @ts-expect-error
+    expect(truncate(-500)).toEqual('');
+    // @ts-expect-error
+    expect(truncate([1, 2, 3])).toEqual('');
+    // @ts-expect-error
+    expect(truncate(new Date())).toEqual('');
+  });
+
+  it('should return passed string value when truncate postion is out of string`s length', () => {
+    expect(truncate('TEST', 10)).toEqual('TEST');
+  });
+
+  it('should return three dots when truncateAt parameter is negative of equal to zero', () => {
+    expect(truncate('TEST', 0)).toEqual('...');
+    expect(truncate('TEST', -10)).toEqual('...');
+  });
+
+  it('should truncate passed string', () => {
+    expect(truncate('TEST', 2)).toEqual('TE...');
+  });
+
+  it('should truncate passed string with custom string', () => {
+    expect(truncate('TEST', 2, '[...]')).toEqual('TE[...]');
+  });
 });
