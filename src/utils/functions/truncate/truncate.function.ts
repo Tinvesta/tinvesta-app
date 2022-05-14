00e76e8c94f5c0853b5001ce0076c7e6a70443e7
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-14 15:05:29 +0400

    test: improve test coverage of truncate function (#300)

--- src/utils/functions/truncate/truncate.function.ts
+++ src/utils/functions/truncate/truncate.function.ts
@@ -1,4 +1,14 @@
-export const truncate = (string: string, truncateAt: number, endsWith: string = '...') => {
+import { isString } from '@utils';
+
+export const truncate = (
+  string: string | null | undefined,
+  truncateAt: number,
+  endsWith: string = '...',
+) => {
+  if (!isString(string)) {
+    return '';
+  }
+
   if (string.length > truncateAt) {
     return `${string.slice(0, Math.max(0, truncateAt))}${endsWith}`;
   }
