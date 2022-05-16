38e5c2819d42bb6278c7fcd0f54f7ecfc59981d0
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-16 19:51:38 +0200

    feat: create internal server error page

--- src/domain/error/error.component.tsx
+++ src/domain/error/error.component.tsx
@@ -2,15 +2,23 @@ import { Typography } from '@mui/material';
 
 import { CenterBlockLayout } from '@ui';
 
+import { useDeviceDetect } from '@utils';
+
 import S from './error.styles';
 import { IErrorProps } from './error.types';
 
-export const Error = ({ children, code, message }: IErrorProps): JSX.Element => (
-  <CenterBlockLayout>
-    <Typography fontWeight={900} variant="h1">
-      {code}
-    </Typography>
-    <Typography variant="h5">{message}</Typography>
-    <S.StyledActionsWrapper>{children}</S.StyledActionsWrapper>
-  </CenterBlockLayout>
-);
+export const Error = ({ children, code, message }: IErrorProps): JSX.Element => {
+  const { deviceData } = useDeviceDetect();
+
+  return (
+    <CenterBlockLayout>
+      <Typography fontWeight={900} variant={deviceData.isSmallerThanXS ? 'h2' : 'h1'}>
+        {code}
+      </Typography>
+      <Typography align="center" variant={deviceData.isSmallerThanXS ? 'body1' : 'h6'}>
+        {message}
+      </Typography>
+      <S.StyledActionsWrapper>{children}</S.StyledActionsWrapper>
+    </CenterBlockLayout>
+  );
+};
