eeeff029bda134c0a893680d5ed6a6e0707b5978
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-16 17:25:47 +0200

    chore: update

--- src/infrastructure/api/health-check/health-check.action.ts
+++ src/infrastructure/api/health-check/health-check.action.ts
@@ -2,5 +2,4 @@ import { nextAxiosInstance } from '@infrastructure';
 
 import { EApiEndpoint } from '@enums';
 
-// eslint-disable-next-line unicorn/consistent-function-scoping
 export const getHealthCheckAction = () => nextAxiosInstance.get(EApiEndpoint.HEALTH_CHECK);
