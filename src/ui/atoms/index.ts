937276b6042d2e014ff70b82393cc65ad2aa260d
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-17 22:57:33 +0400

    refactor: remove horizontal button group component (#331)

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -5,7 +5,6 @@ export * from './swiper/swiper.component';
 export * from './autocomplete/autocomplete.component';
 export * from './upload-images/upload-images.component';
 export * from './location-autocomplete/location-autocomplete.component';
-export * from './horizontal-button-group/horizontal-button-group.component';
 export * from './linear-progress-with-label/linear-progress-with-label.component';
 export * from './profile-details-preview-label/profile-details-preview-label.component';
 
@@ -15,7 +14,6 @@ export * from './swiper/swiper.types';
 export * from './autocomplete/autocomplete.types';
 export * from './upload-images/upload-images.types';
 export * from './location-autocomplete/location-autocomplete.types';
-export * from './horizontal-button-group/horizontal-button-group.types';
 export * from './linear-progress-with-label/linear-progress-with-label.types';
 export * from './profile-details-preview-label/profile-details-preview-label.types';
 
