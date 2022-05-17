86f89a544b0261d7309ba78ec838ba0d4697dca9
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-17 17:24:49 +0200

    refactor: remove react tsparticles

--- src/application/pages/release-date/release-date.page.tsx
+++ src/application/pages/release-date/release-date.page.tsx
@@ -2,8 +2,6 @@ import { Typography } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import { ParticlesBackground } from '@ui';
-
 import S from './release-date.styles';
 
 export const ReleaseDatePage = (): JSX.Element => (
@@ -12,7 +10,6 @@ export const ReleaseDatePage = (): JSX.Element => (
       <title>Tinvesta</title>
       <meta content="Tinvesta app" name="description" />
     </Head>
-    <ParticlesBackground />
     <Image priority alt="Tinvesta" height={200} src="/images/animated-full-logo.svg" width={200} />
     <Typography variant="h3">28.05.2022</Typography>
   </S.StyledWrapper>
