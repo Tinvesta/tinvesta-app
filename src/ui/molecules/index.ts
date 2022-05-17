7f07d13ec1fd65371067051a1c4498a327392fcb
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-17 20:49:29 +0200

    refactor: remove horizontal button group component

--- src/ui/molecules/index.ts
+++ src/ui/molecules/index.ts
@@ -7,7 +7,6 @@ export * from './profile-card-action-buttons/profile-card-action-buttons.compone
 export * from './autocomplete-with-controller/autocomplete-with-controller.component';
 export * from './upload-images-with-controller/upload-images-with-controller.component';
 export * from './location-autocomplete-with-controller/location-autocomplete-with-controller.component';
-export * from './horizontal-button-group-with-controller/horizontal-button-group-with-controller.component';
 
 export * from './confirmation-modal/confirmation-modal.types';
 export * from './select-with-controller/select-with-controller.types';
@@ -15,4 +14,3 @@ export * from './text-field-with-controller/text-field-with-controller.types';
 export * from './autocomplete-with-controller/autocomplete-with-controller.types';
 export * from './upload-images-with-controller/upload-images-with-controller.types';
 export * from './location-autocomplete-with-controller/location-autocomplete-with-controller.types';
-export * from './horizontal-button-group-with-controller/horizontal-button-group-with-controller.types';
