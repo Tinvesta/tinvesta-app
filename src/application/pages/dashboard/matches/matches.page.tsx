f5479b29c53eaab2a7477cf138d27059a42e0984
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-18 20:04:19 +0200

    feat: render matches pairs

--- src/application/pages/dashboard/matches/matches.page.tsx
+++ src/application/pages/dashboard/matches/matches.page.tsx
@@ -7,11 +7,26 @@ import { Loading } from '@ui';
 
 import { useDeviceDetect, useUser } from '@utils';
 
+import { supabaseInstance } from '@infrastructure';
+
 import { ERoutes } from '@enums';
 
-import { DesktopDashboardLayout, MobileDashboardLayout } from '../layouts';
+import {
+  IFocusMarket,
+  IIndustrialSector,
+  IInvestmentSize,
+  IInvestmentStageType,
+  IInvestorDemandType,
+  IInvestorProfileType,
+  IStartupProfileCreatorType,
+  IStartupSector,
+  ITeamSize,
+} from '@interfaces';
 
-export const MatchesPage = (): JSX.Element => {
+import { DesktopDashboardLayout, MobileDashboardLayout } from '../layouts';
+import { IMatchesProps } from './matches.types';
+
+export const MatchesPage = (props: IMatchesProps): JSX.Element => {
   const router = useRouter();
   const { isLoading, user } = useUser();
   const { deviceData } = useDeviceDetect();
@@ -44,7 +59,47 @@ export const MatchesPage = (): JSX.Element => {
 
   return (
     <DashboardLayout>
-      <Matches clientTypeId={user.client_type_id as number} />
+      <Matches clientTypeId={user.client_type_id as number} {...props} />
     </DashboardLayout>
   );
+};
+
+export const getStaticProps = async () => {
+  const [
+    { data: teamSizes },
+    { data: focusMarkets },
+    { data: startupSectors },
+    { data: investmentSizes },
+    { data: industrialSectors },
+    { data: investorDemandTypes },
+    { data: investmentStageTypes },
+    { data: investorProfileTypes },
+    { data: startupProfileCreatorTypes },
+  ] = await Promise.all([
+    supabaseInstance.from<ITeamSize>('team_sizes').select('id,name'),
+    supabaseInstance.from<IFocusMarket>('focus_markets').select('id,name'),
+    supabaseInstance.from<IStartupSector>('startup_sectors').select('id,name'),
+    supabaseInstance.from<IInvestmentSize>('investment_sizes').select('id,name'),
+    supabaseInstance.from<IIndustrialSector>('industrial_sectors').select('id,name'),
+    supabaseInstance.from<IInvestorDemandType>('investor_demand_types').select('id,name'),
+    supabaseInstance.from<IInvestmentStageType>('investment_stage_types').select('id,name'),
+    supabaseInstance.from<IInvestorProfileType>('investor_profile_types').select('id,name'),
+    supabaseInstance
+      .from<IStartupProfileCreatorType>('startup_profile_creator_types')
+      .select('id,name'),
+  ]);
+
+  return {
+    props: {
+      teamSizes,
+      focusMarkets,
+      startupSectors,
+      investmentSizes,
+      industrialSectors,
+      investorDemandTypes,
+      investmentStageTypes,
+      investorProfileTypes,
+      startupProfileCreatorTypes,
+    },
+  };
 };
--- src/application/pages/dashboard/matches/matches.types.ts
+++ src/application/pages/dashboard/matches/matches.types.ts
@@ -1,1 +1,23 @@
-export interface IMatchesProps {}
+import {
+  IFocusMarket,
+  IIndustrialSector,
+  IInvestmentSize,
+  IInvestmentStageType,
+  IInvestorDemandType,
+  IInvestorProfileType,
+  IStartupProfileCreatorType,
+  IStartupSector,
+  ITeamSize,
+} from '@interfaces';
+
+export interface IMatchesProps {
+  focusMarkets: IFocusMarket[];
+  industrialSectors: IIndustrialSector[];
+  investmentSizes: IInvestmentSize[];
+  investmentStageTypes: IInvestmentStageType[];
+  investorDemandTypes: IInvestorDemandType[];
+  investorProfileTypes: IInvestorProfileType[];
+  startupProfileCreatorTypes: IStartupProfileCreatorType[];
+  startupSectors: IStartupSector[];
+  teamSizes: ITeamSize[];
+}
