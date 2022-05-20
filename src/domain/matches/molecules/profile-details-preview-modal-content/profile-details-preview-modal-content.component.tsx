96c301e5f192a58cb9d11f19ca12db7e42180c35
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-20 17:06:56 +0400

    feat: create action button on match preview (#346)

--- src/domain/matches/molecules/profile-details-preview-modal-content/profile-details-preview-modal-content.component.tsx
+++ src/domain/matches/molecules/profile-details-preview-modal-content/profile-details-preview-modal-content.component.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@mui/material';
+
 import { ProfileDetailsPreview } from '@ui';
 
 import { useDeviceDetect } from '@utils';
@@ -12,6 +14,10 @@ export const ProfileDetailsPreviewModalContent = ({
 }: IProfileDetailsPreviewModalContentProps): JSX.Element => {
   const { deviceData } = useDeviceDetect();
 
+  const sendEmail = () =>
+    // @ts-expect-error
+    window.open(`mailto:${selectedProfile.contactEmail}?subject=Tinvesta -`);
+
   return (
     <S.StyledWrapper>
       <S.StyledHeader>
@@ -25,6 +31,14 @@ export const ProfileDetailsPreviewModalContent = ({
         // @ts-expect-error
         profileDetails={selectedProfile}
       />
+      <S.StyledActionsWrapper>
+        <Button color="error" size="large" variant="outlined">
+          Remove match
+        </Button>
+        <Button color="secondary" size="large" variant="contained" onClick={sendEmail}>
+          Send an email
+        </Button>
+      </S.StyledActionsWrapper>
     </S.StyledWrapper>
   );
 };
