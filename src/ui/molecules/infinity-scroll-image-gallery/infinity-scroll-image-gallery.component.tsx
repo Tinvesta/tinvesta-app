91b7c50b44e444f8f88f15f5ffc08b7efcad8b51
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-21 14:48:59 +0200

    feat: filter out match instead of refetching

--- src/ui/molecules/infinity-scroll-image-gallery/infinity-scroll-image-gallery.component.tsx
+++ src/ui/molecules/infinity-scroll-image-gallery/infinity-scroll-image-gallery.component.tsx
@@ -1,9 +1,9 @@
 import { debounce } from 'debounce';
-import { Children, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { Loading } from '@ui';
 
-import { useEventListener, usePrevious } from '@utils';
+import { useEventListener } from '@utils';
 
 import S from './infinity-scroll-image-gallery.styles';
 import { IInfinityScrollImageGalleryProps } from './infinity-scroll-image-gallery.types';
@@ -17,20 +17,6 @@ export const InfinityScrollImageGallery = ({
   shouldLoadMore = true,
 }: IInfinityScrollImageGalleryProps): JSX.Element => {
   const [page, setPage] = useState(initialPage);
-
-  const childrenLength = Children.toArray(children).length;
-  const previousItemsLength = usePrevious(childrenLength);
-
-  useEffect(() => {
-    if (
-      previousItemsLength !== undefined &&
-      childrenLength === 0 &&
-      previousItemsLength !== childrenLength
-    ) {
-      loadMore(0);
-      setPage(initialPage);
-    }
-  }, [previousItemsLength, childrenLength]);
 
   const handleScroll = debounce((event: Event) => {
     if (!event.target || isLoading || !shouldLoadMore) {
