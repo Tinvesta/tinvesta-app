425c27881c6c3e972f43eeee6d8a7892b8e457e2
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-22 12:05:20 +0200

    chore: update

--- src/domain/profile/atoms/feedback-form/feedback-form.defaults.ts
+++ src/domain/profile/atoms/feedback-form/feedback-form.defaults.ts
@@ -5,7 +5,6 @@ export const translationStrings = [
   'common.form.field.error.contain.enters.or.spaces',
   'common.form.field.error.starts.or.ends.with.whitespace',
 
-  'component.dashboard.feedback.form.rating.field.label',
   'component.dashboard.feedback.form.message.field.label',
   'component.dashboard.feedback.form.message.field.max.length.error',
 ] as const;
