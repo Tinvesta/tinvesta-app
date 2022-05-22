c2fa58603a08e9324c5a1bfd0e048a212a3d7650
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-22 14:09:30 +0400

    feat: cleanup profile feedback section (#372)

--- src/infrastructure/i18n/languages/en/dashboard/profile/feedback-form.translations.ts
+++ src/infrastructure/i18n/languages/en/dashboard/profile/feedback-form.translations.ts
@@ -1,7 +1,9 @@
 export default {
   // General
   'component.dashboard.feedback.form.heading': 'Feedback',
-  'component.dashboard.feedback.form.messages.success': 'Thank you for your feedback :)',
+  'component.dashboard.feedback.form.messages.success': 'Thank you for your feedback!',
+  'component.dashboard.feedback.form.description':
+    'I appreciate any feedback regards general app feeling or even bug report!',
 
   // Message
   'component.dashboard.feedback.form.message.field.label': 'Message',
