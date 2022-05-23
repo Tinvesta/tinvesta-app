d54826823c2000344edbc34424fd101df0418c86
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-23 20:18:25 +0200

    feat: create use copy to clipboard hook

--- src/ui/hooks/index.ts
+++ src/ui/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './use-modal/use-modal.hook';
+export * from './use-copy-to-clipboard/use-copy-to-clipboard.hook';
 
 export * from './use-modal/use-modal.types';
---
+++ src/ui/hooks/use-copy-to-clipboard/use-copy-to-clipboard.spec.ts
@@ -0,0 +1,7 @@
+import { functionImportTest } from '@utils';
+
+import { useCopyToClipboard } from './use-copy-to-clipboard.hook';
+
+describe('useCopyToClipboard hook', () => {
+  functionImportTest(useCopyToClipboard);
+});
