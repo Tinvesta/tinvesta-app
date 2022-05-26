33956756b4617b20403300549bd0d159cc0a8605
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-26 19:55:58 +0200

    chore: update

--- src/domain/onboarding/atoms/desktop-onboarding-form-layout/desktop-onboarding-form-layout.layout.tsx
+++ src/domain/onboarding/atoms/desktop-onboarding-form-layout/desktop-onboarding-form-layout.layout.tsx
@@ -6,8 +6,6 @@ import { memo } from 'react';
 
 import { CenterBlockLayout } from '@ui';
 
-import { useDeviceDetect } from '@utils';
-
 import S from './desktop-onboarding-form-layout.styles';
 import { IDesktopOnboardingFormLayoutProps } from './desktop-onboarding-form-layout.types';
 
@@ -22,77 +20,68 @@ const DesktopOnboardingFormLayoutComponent = ({
   onBackButtonClick,
   subHeading,
   ...formProps
-}: IDesktopOnboardingFormLayoutProps): JSX.Element => {
-  const { deviceData } = useDeviceDetect();
-
-  return (
-    <CenterBlockLayout>
-      <Image
-        priority
-        alt="Tinvesta"
-        layout="fill"
-        objectFit="cover"
-        src="/images/background/desktop-onboarding.svg"
-      />
-      <S.StyledWrapper>
-        <S.StyledContentWrapper>
-          {subHeading && <S.StyledHeading variant="body1">{subHeading}</S.StyledHeading>}
-          {heading && (
-            <S.StyledHeading fontWeight={700} variant="h3">
-              {heading}
-            </S.StyledHeading>
-          )}
-          <S.StyledFormWrapper {...formProps}>
+}: IDesktopOnboardingFormLayoutProps): JSX.Element => (
+  <CenterBlockLayout>
+    <Image
+      priority
+      alt="Tinvesta"
+      layout="fill"
+      objectFit="cover"
+      src="/images/background/desktop-onboarding.svg"
+    />
+    <S.StyledWrapper>
+      <S.StyledContentWrapper>
+        {subHeading && <S.StyledHeading variant="body1">{subHeading}</S.StyledHeading>}
+        {heading && (
+          <S.StyledHeading fontWeight={700} variant="h3">
+            {heading}
+          </S.StyledHeading>
+        )}
+        <S.StyledFormWrapper {...formProps}>
+          <Grid container columnSpacing={5} justifyContent="center" rowSpacing={3}>
+            {children}
             <Grid
-              container
-              columnSpacing={deviceData.isSmallerThanXS ? 4 : 5}
-              justifyContent="center"
-              rowSpacing={deviceData.isSmallerThanMD ? 1 : 3}
+              item
+              display="flex"
+              gap={5}
+              justifyContent={centerActionButtons ? 'center' : 'flex-end'}
+              xs={12}
             >
-              {children}
-              <Grid
-                item
-                display="flex"
-                gap={deviceData.isSmallerThanXS ? 4 : 5}
-                justifyContent={centerActionButtons ? 'center' : 'flex-end'}
-                xs={12}
-              >
-                {backButtonText && (
-                  <Grid item xs={3}>
-                    <LoadingButton
-                      fullWidth
-                      color="secondary"
-                      disabled={isLoading}
-                      size="large"
-                      startIcon={addArrowToBackButton && <ArrowBackIcon />}
-                      variant="outlined"
-                      onClick={onBackButtonClick}
-                    >
-                      {backButtonText}
-                    </LoadingButton>
-                  </Grid>
-                )}
-                {continueButtonText && (
-                  <Grid item xs={3}>
-                    <LoadingButton
-                      fullWidth
-                      color="secondary"
-                      loading={isLoading}
-                      size="large"
-                      type="submit"
-                      variant="contained"
-                    >
-                      {continueButtonText}
-                    </LoadingButton>
-                  </Grid>
-                )}
-              </Grid>
+              {backButtonText && (
+                <Grid item xs={3}>
+                  <LoadingButton
+                    fullWidth
+                    color="secondary"
+                    disabled={isLoading}
+                    size="large"
+                    startIcon={addArrowToBackButton && <ArrowBackIcon />}
+                    variant="outlined"
+                    onClick={onBackButtonClick}
+                  >
+                    {backButtonText}
+                  </LoadingButton>
+                </Grid>
+              )}
+              {continueButtonText && (
+                <Grid item xs={3}>
+                  <LoadingButton
+                    fullWidth
+                    color="secondary"
+                    loading={isLoading}
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    {continueButtonText}
+                  </LoadingButton>
+                </Grid>
+              )}
             </Grid>
-          </S.StyledFormWrapper>
-        </S.StyledContentWrapper>
-      </S.StyledWrapper>
-    </CenterBlockLayout>
-  );
-};
+          </Grid>
+        </S.StyledFormWrapper>
+      </S.StyledContentWrapper>
+    </S.StyledWrapper>
+  </CenterBlockLayout>
+);
 
 export const DesktopOnboardingFormLayout = memo(DesktopOnboardingFormLayoutComponent);
