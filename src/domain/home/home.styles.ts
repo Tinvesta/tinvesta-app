1bee250c671aee1ba045f9d513c3d330c033702a
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-27 21:29:30 +0200

    feat: improve desktop homepage styles

--- src/domain/home/home.styles.ts
+++ src/domain/home/home.styles.ts
@@ -1,16 +1,27 @@
 import styled from '@emotion/styled';
 
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const StyledSubHeaderWrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  justify-content: center;
 
   > .MuiButton-root {
-    margin-top: ${({ theme }) => theme.spacing(5)};
+    margin-top: ${({ theme }) => theme.spacing(12)};
   }
 `;
 
 const S = {
+  StyledWrapper,
   StyledSubHeaderWrapper,
 };
 
