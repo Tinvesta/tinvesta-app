4e63362e7c85ebfc7b091242634e91b8c2a05108
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-05-27 16:05:54 +0400

    chore(deps): bump test renovate deps (#414)

--- src/utils/guards/is-function/is-function.function.ts
+++ src/utils/guards/is-function/is-function.function.ts
@@ -1,7 +1,7 @@
 export const isFunction = (functionToCheck: unknown): functionToCheck is Function =>
   !!(
     typeof functionToCheck === 'function' &&
-    functionToCheck.constructor &&
-    functionToCheck.call &&
-    functionToCheck.apply
+    !!functionToCheck.constructor &&
+    !!functionToCheck.call &&
+    !!functionToCheck.apply
   );
