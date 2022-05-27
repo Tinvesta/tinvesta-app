5f24dbf187c69e51fd3b0a74d7d634f0ce704360
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-27 18:11:44 +0200

    feat: update confirmation modal styles

--- src/ui/molecules/confirmation-modal/confirmation-modal.component.tsx
+++ src/ui/molecules/confirmation-modal/confirmation-modal.component.tsx
@@ -53,7 +53,7 @@ export const ConfirmationModal = ({
                 fullWidth
                 {...cancellationButtonProps}
                 color="secondary"
-                size={deviceData.isSmallerThanXS ? 'small' : 'medium'}
+                size={deviceData.isSmallerThanXS ? 'medium' : 'large'}
                 variant="outlined"
                 onClick={onCancel}
               >
@@ -64,7 +64,7 @@ export const ConfirmationModal = ({
               <Button
                 fullWidth
                 color="secondary"
-                size={deviceData.isSmallerThanXS ? 'small' : 'medium'}
+                size={deviceData.isSmallerThanXS ? 'medium' : 'large'}
                 variant="contained"
                 {...confirmationButtonProps}
                 onClick={onConfirm}
