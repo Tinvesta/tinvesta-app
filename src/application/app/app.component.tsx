f421b05559d9f58f01e285dbf54222b915efb210
playerony <pawel.wojtasinski.1995@gmail.com>
2022-05-28 14:12:54 +0200

    feat: display content when its laoded

--- src/application/app/app.component.tsx
+++ src/application/app/app.component.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import {
   CacheProvider,
   ConfirmationModalProvider,
@@ -9,26 +11,36 @@ import {
   UserProvider,
 } from '@application';
 
+import { Loader } from '@ui/atoms';
+
 import { IAppProps } from './app.types';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-export const App = ({ Component, emotionCache, pageProps }: IAppProps) => (
-  <ThemeProvider>
-    <QueryParamProvider>
-      <CacheProvider emotionCache={emotionCache}>
-        <ToastProvider>
-          <LocaleProvider>
-            <QueryClientProvider>
-              <ConfirmationModalProvider>
-                <UserProvider>
-                  <Component {...pageProps} />
-                </UserProvider>
-              </ConfirmationModalProvider>
-            </QueryClientProvider>
-          </LocaleProvider>
-        </ToastProvider>
-      </CacheProvider>
-    </QueryParamProvider>
-  </ThemeProvider>
-);
+export const App = ({ Component, emotionCache, pageProps }: IAppProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <QueryParamProvider>
+        <CacheProvider emotionCache={emotionCache}>
+          <ToastProvider>
+            <LocaleProvider>
+              <QueryClientProvider>
+                <ConfirmationModalProvider>
+                  <UserProvider>
+                    {isLoading ? <Loader /> : <Component {...pageProps} />}
+                  </UserProvider>
+                </ConfirmationModalProvider>
+              </QueryClientProvider>
+            </LocaleProvider>
+          </ToastProvider>
+        </CacheProvider>
+      </QueryParamProvider>
+    </ThemeProvider>
+  );
+};
