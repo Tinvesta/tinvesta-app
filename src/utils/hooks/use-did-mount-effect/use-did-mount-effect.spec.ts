1b0f11df4e4f341a1ead4164f268a770adc2938f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-02 21:56:45 +0200

    feat: remove use did mount effect

--- src/utils/hooks/use-did-mount-effect/use-did-mount-effect.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@utils';
-
-import { useDidMountEffect } from './use-did-mount-effect.hook';
-
-describe('useDidMountEffect hook', () => {
-  functionImportTest(useDidMountEffect);
-});
