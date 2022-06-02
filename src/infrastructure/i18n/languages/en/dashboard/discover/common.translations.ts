118e4b9e92dd6d83713167069f7648a2a373d60e
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-06-02 21:08:43 +0400

    feat: display empty component when there are no more records (#492)

--- src/infrastructure/i18n/languages/en/dashboard/discover/common.translations.ts
+++ src/infrastructure/i18n/languages/en/dashboard/discover/common.translations.ts
@@ -3,4 +3,10 @@ export default {
   'component.dashboard.discover.likes.limit.label':
     'You have reached you daily limit of likes ({{0}}). Purchase subscription to remove the limit',
   'component.dashboard.discover.likes.limit.action.button': 'Check subscription options',
+
+  // No more records to discover
+  'component.dashboard.discover.no.more.records.label.investor':
+    'There are no more startups to discover',
+  'component.dashboard.discover.no.more.records.label.startup':
+    'There are no more investors to discover',
 };
