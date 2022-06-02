c2f62d905159a775bff5bd44efb27ce0937894d0
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-06-02 18:26:34 +0400

    chore: reset local changes after release (#487)

--- scripts/shell/release-develop-to-main.sh
+++ scripts/shell/release-develop-to-main.sh
@@ -165,3 +165,21 @@ open $GITHUB_PULL_REQUESTS_URL
 # cleanup
 git checkout $DEVELOP_BRANCH_NAME
 git branch -D $releaseBranchName
+
+# git-reset-local-changes
+currentBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# To remove untracked files
+git clean -f
+
+# To remove untracked directories
+git clean -fd
+
+# revert changes to index
+git reset --hard
+
+# remove all local commits
+git fetch origin
+git reset --hard origin/$currentBranch
+
+git fetch --all
