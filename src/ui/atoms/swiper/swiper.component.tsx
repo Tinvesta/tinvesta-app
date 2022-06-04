f4fa9cb50c62c162e900d78cc0e067b56cd12ca7
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-04 13:23:17 +0200

    feat: remove unused elements from swiper

--- src/ui/atoms/swiper/swiper.component.tsx
+++ src/ui/atoms/swiper/swiper.component.tsx
@@ -1,58 +1,15 @@
 import { memo } from 'react';
-import {
-  Autoplay,
-  Controller,
-  EffectCards,
-  EffectCoverflow,
-  EffectCreative,
-  EffectCube,
-  EffectFade,
-  EffectFlip,
-  FreeMode,
-  Grid,
-  HashNavigation,
-  History,
-  Keyboard,
-  Lazy,
-  Manipulation,
-  Mousewheel,
-  Navigation,
-  Pagination,
-  Parallax,
-  Scrollbar,
-  Thumbs,
-  Virtual,
-  Zoom,
-} from 'swiper';
+import { Keyboard, Navigation, Pagination, Scrollbar } from 'swiper';
 import 'swiper/css/bundle';
 import { Swiper as ReactSwiper } from 'swiper/react';
 
 import { ISwiperProps, TSwiperModulesObject } from './swiper.types';
 
 const swiperModulesMapper: TSwiperModulesObject = {
-  lazy: Lazy,
-  zoom: Zoom,
-  grid: Grid,
-  thumbs: Thumbs,
-  history: History,
-  virtual: Virtual,
-  autoplay: Autoplay,
   keyboard: Keyboard,
-  parallax: Parallax,
-  freeMode: FreeMode,
   scrollbar: Scrollbar,
-  controller: Controller,
-  effectCube: EffectCube,
-  effectFade: EffectFade,
-  effectFlip: EffectFlip,
-  mousewheel: Mousewheel,
-  navigation: Navigation,
   pagination: Pagination,
-  effectCards: EffectCards,
-  manipulation: Manipulation,
-  effectCreative: EffectCreative,
-  hashNavigation: HashNavigation,
-  effectCoverflow: EffectCoverflow,
+  navigation: Navigation,
 };
 
 const SwiperComponent = ({ children, modules = [], ...restProps }: ISwiperProps) => (
