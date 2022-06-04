b7c243067fa2ef2e480fef617fc329b7f2d2b7a9
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-04 13:34:54 +0200

    feat: uppercase profile details preview label

--- src/ui/molecules/profile-details-preview/parts/profile-details-preview-label/profile-details-preview-label.component.tsx
+++ src/ui/molecules/profile-details-preview/parts/profile-details-preview-label/profile-details-preview-label.component.tsx
@@ -19,7 +19,12 @@ export const ProfileDetailsPreviewLabel = ({
     <S.StyledWrapper>
       <S.StyledHeader>
         <Icon color="secondary">{icon}</Icon>
-        <Typography color="secondary" fontWeight={900} variant="body2">
+        <Typography
+          color="secondary"
+          fontWeight={900}
+          sx={{ textTransform: 'uppercase' }}
+          variant="body2"
+        >
           {label}
         </Typography>
       </S.StyledHeader>
