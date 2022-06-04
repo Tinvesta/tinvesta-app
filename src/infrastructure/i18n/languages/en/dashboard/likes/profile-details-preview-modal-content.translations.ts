0b7d9f6b27774bd7797232e85cfc431c362d1a87
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-04 16:48:59 +0200

    chore: update translation

--- src/infrastructure/i18n/languages/en/dashboard/likes/profile-details-preview-modal-content.translations.ts
+++ src/infrastructure/i18n/languages/en/dashboard/likes/profile-details-preview-modal-content.translations.ts
@@ -1,4 +1,5 @@
 export default {
-  'component.dashboard.likes.profile.details.preview.modal.content.like.button': 'Like',
+  'component.dashboard.likes.profile.details.preview.modal.content.like.button':
+    'Like this profile',
   'component.dashboard.likes.profile.details.preview.modal.content.dislike.button': 'Remove a like',
 };
