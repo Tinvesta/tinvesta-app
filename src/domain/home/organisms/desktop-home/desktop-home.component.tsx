3c23f19abef0841a29498608b357daa5be29abf3
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-06 16:48:22 +0200

    feat: add components for mobile and desktop home

---
+++ src/domain/home/organisms/desktop-home/desktop-home.component.tsx
@@ -0,0 +1,3 @@
+import S from './desktop-home.styles';
+
+export const DesktopHome = (): JSX.Element => <S.StyledWrapper>DesktopHome</S.StyledWrapper>;
---
+++ src/domain/home/organisms/index.ts
@@ -0,0 +1,2 @@
+export * from './mobile-home/mobile-home.component';
+export * from './desktop-home/desktop-home.component';
