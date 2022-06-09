75cb12a7c9620a03c2ade9dcaa3869da0c833e2f
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-06-09 18:59:41 +0400

    feat: create mobile home component (#567)

--- src/domain/home/molecules/mobile-home/mobile-home.component.tsx
+++ src/domain/home/molecules/mobile-home/mobile-home.component.tsx
@@ -1,3 +1,87 @@
-import S from './mobile-home.styles';
+import { Button } from '@mui/material';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-export const MobileHome = (): JSX.Element => <S.StyledWrapper>MobileHome</S.StyledWrapper>;
+import { HeaderAndFooterLayout, useModal } from '@ui';
+
+import { useDeviceDetect, useTranslation } from '@utils';
+
+import { ERoutes } from '@enums';
+
+import { SignInModalContent } from '../../atoms';
+import { translationStrings } from './mobile-home.defaults';
+import S from './mobile-home.styles';
+import { IMobileHomeProps } from './mobile-home.types';
+
+export const MobileHome = ({ clientTypeId, isSignedIn }: IMobileHomeProps): JSX.Element => {
+  const router = useRouter();
+  const { Modal, show } = useModal();
+  const { deviceData } = useDeviceDetect();
+  const translations = useTranslation(translationStrings);
+
+  const onSignInButtonClick = () => {
+    if (!isSignedIn) {
+      return show();
+    }
+
+    router.push(clientTypeId ? ERoutes.DASHBOARD : ERoutes.ONBOARDING);
+  };
+
+  return (
+    <HeaderAndFooterLayout openLoginModal={show}>
+      <S.StyledWrapper>
+        <Modal title="Create account">
+          <SignInModalContent />
+        </Modal>
+        <S.StyledTextBlockWrapper>
+          <S.StyledHeader
+            fontWeight={700}
+            textAlign="left"
+            variant={deviceData.isSmallerThanXS ? 'h3' : 'h2'}
+          >
+            {translations.componentHomeHeader}
+          </S.StyledHeader>
+          <S.StyledSubHeaderWrapper>
+            <S.StyledSubheader
+              align="center"
+              fontWeight={700}
+              variant={deviceData.isSmallerThanXS ? 'body2' : 'body1'}
+            >
+              {translations.componentHomeSubheader}
+            </S.StyledSubheader>
+            <Button
+              color="secondary"
+              size={deviceData.isSmallerThanXS ? 'medium' : 'large'}
+              variant="contained"
+              onClick={onSignInButtonClick}
+            >
+              {translations.componentHomeButtonLabel}
+            </Button>
+          </S.StyledSubHeaderWrapper>
+        </S.StyledTextBlockWrapper>
+        <S.StyledCardsWrapper>
+          <S.StyledMatchCardWrapper>
+            <Image
+              priority
+              alt="Tinvesta"
+              height={724}
+              objectFit="fill"
+              src="/images/match-card.png"
+              width={624}
+            />
+          </S.StyledMatchCardWrapper>
+          <S.StyledMaskCardWrapper>
+            <Image
+              priority
+              alt="Tinvesta"
+              height={724}
+              objectFit="fill"
+              src="/images/match-mask.png"
+              width={624}
+            />
+          </S.StyledMaskCardWrapper>
+        </S.StyledCardsWrapper>
+      </S.StyledWrapper>
+    </HeaderAndFooterLayout>
+  );
+};
