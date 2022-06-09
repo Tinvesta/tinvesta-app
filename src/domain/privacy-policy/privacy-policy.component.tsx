486c80e0b12d61d664aff694d8020d7251977198
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-06-09 22:02:33 +0400

    feat: add header and footer layout for terms and privacy (#572)

--- src/domain/privacy-policy/privacy-policy.component.tsx
+++ src/domain/privacy-policy/privacy-policy.component.tsx
@@ -1,3 +1,11 @@
+import { HeaderAndFooterLayout } from '@ui';
+
 import S from './privacy-policy.styles';
 
-export const PrivacyPolicy = (): JSX.Element => <S.StyledWrapper>PrivacyPolicy</S.StyledWrapper>;
+export const PrivacyPolicy = (): JSX.Element => (
+  <HeaderAndFooterLayout openLoginModal={() => console.log('Privacy policy')}>
+    <S.StyledWrapper>
+      <S.StyledContentWrapper>Privacy policy</S.StyledContentWrapper>
+    </S.StyledWrapper>
+  </HeaderAndFooterLayout>
+);
--- src/domain/privacy-policy/privacy-policy.styles.ts
+++ src/domain/privacy-policy/privacy-policy.styles.ts
@@ -1,9 +1,33 @@
 import styled from '@emotion/styled';
 
-const StyledWrapper = styled.div``;
+import { respondToMax } from '@infrastructure';
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing(10, 0)};
+`;
+
+const StyledContentWrapper = styled.div`
+  width: 95%;
+  height: 100%;
+  display: flex;
+  margin: 0 auto;
+  max-width: 1300px;
+  align-items: center;
+  justify-content: space-between;
+
+  ${respondToMax.medium`
+    max-width: unset;
+  `}
+
+  ${({ theme }) => respondToMax.xmobile`
+    padding-right: ${theme.spacing(3)};
+  `}
+`;
 
 const S = {
   StyledWrapper,
+  StyledContentWrapper,
 };
 
 export default S;
