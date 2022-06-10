01b878fb696264dc63c8fc383b35edca1bbad395
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-10 14:30:20 +0200

    feat: correct login modal styles

--- src/domain/home/atoms/login-modal-content/login-modal-content.styles.ts
+++ src/domain/home/atoms/login-modal-content/login-modal-content.styles.ts
@@ -4,13 +4,13 @@ const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.spacing(5)};
-  padding-top: ${({ theme }) => theme.spacing(5)};
+  padding-top: ${({ theme }) => theme.spacing(4)};
 `;
 
 const StyledButtonsWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${({ theme }) => theme.spacing(3)};
+  gap: ${({ theme }) => theme.spacing(4)};
 `;
 
 const S = {
