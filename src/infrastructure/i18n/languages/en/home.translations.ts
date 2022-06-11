08190be51fd49c7df65e5ec933a914a45db88f74
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-11 17:51:53 +0200

    chore: change home button label

--- src/infrastructure/i18n/languages/en/home.translations.ts
+++ src/infrastructure/i18n/languages/en/home.translations.ts
@@ -1,7 +1,7 @@
 export default {
   // General
   'component.home.header': 'Swipe & Match',
-  'component.home.button.label': 'Create account',
+  'component.home.button.label': "Start matching - It's free!",
   'component.home.subheader':
     'Matchmaking app for startups and investors all over the world. The app enables investors and startups to find each other in the easiest way: create a profile - swipe - match.',
 
