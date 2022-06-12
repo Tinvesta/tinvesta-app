e160de7a21be51c2893c19bb1d1e1c56ad3a8d54
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-12 14:15:53 +0200

    feat: add back to top button

--- src/ui/layout/header-and-footer-layout/header-and-footer-layout.styles.ts
+++ src/ui/layout/header-and-footer-layout/header-and-footer-layout.styles.ts
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
+import { respondToMax } from '@infrastructure';
 
 const StyledWrapper = styled.div`
   width: 100%;
@@ -7,8 +10,35 @@ const StyledWrapper = styled.div`
   position: relative;
 `;
 
+const StyledBackToTopButtonWrapper = styled(motion.div)`
+  position: fixed;
+  right: ${({ theme }) => theme.spacing(10)};
+  bottom: ${({ theme }) => theme.spacing(10)};
+
+  ${({ theme }) => respondToMax.medium`
+    right: ${theme.spacing(8)};
+    bottom: ${theme.spacing(8)};
+  `}
+
+  ${({ theme }) => respondToMax.small`
+    right: ${theme.spacing(7)};
+    bottom: ${theme.spacing(7)};
+  `}
+
+  ${({ theme }) => respondToMax.mobile`
+    right: ${theme.spacing(6)};
+    bottom: ${theme.spacing(6)};
+  `}
+
+  ${({ theme }) => respondToMax.xmobile`
+    right: ${theme.spacing(5)};
+    bottom: ${theme.spacing(5)};
+  `}
+`;
+
 const S = {
   StyledWrapper,
+  StyledBackToTopButtonWrapper,
 };
 
 export default S;
