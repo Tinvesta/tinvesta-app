00da75bbd2dfa92d9d2bde1cb927d5815119c6d1
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-13 10:44:18 +0200

    chore: update

--- src/ui/atoms/footer/footer.component.tsx
+++ src/ui/atoms/footer/footer.component.tsx
@@ -29,7 +29,7 @@ export const Footer = (): JSX.Element => {
             alt="Tinvesta - One&#0032;swipe&#0032;away&#0032;from&#0032;finding&#0032;the&#0032;new&#0032;business&#0032;partner | Product Hunt"
             height="54"
             src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=349272&theme=light"
-            style={{ width: '250px', height: '54px' }}
+            style={{ width: '250px', height: '54px', paddingBottom: '10px' }}
             width="250"
           />
         </a>
