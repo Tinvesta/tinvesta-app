9d7facaa3bfc68a2d954046f0d5ce5ac8e1293e1
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-18 16:41:15 +0200

    refactor: replace get window height with my package

--- src/utils/functions/get-window-height/get-window-height.spec.ts
+++
@@ -1,9 +0,0 @@
-import { functionImportTest, getWindowHeight } from '@utils';
-
-describe('getWindowHeight function', () => {
-  functionImportTest(getWindowHeight);
-
-  it('should return window width', () => {
-    expect(getWindowHeight()).toEqual(768);
-  });
-});
