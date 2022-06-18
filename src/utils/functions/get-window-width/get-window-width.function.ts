90e653fdda281931b164d01e071153db275b080f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-18 15:17:08 +0200

    refactor: replace get window size with my package

--- src/utils/functions/get-window-width/get-window-width.function.ts
+++
@@ -1,4 +0,0 @@
-export const getWindowWidth = (): number =>
-  (typeof window !== 'undefined' &&
-    (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth)) ||
-  0;
