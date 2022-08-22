951d2d6010a0891d72e88210daffbb3eecd52a62
playerony <pawel.wojtasinski.1995@gmail.com>
2022-08-22 12:18:03 +0200

    chore: update

--- src/infrastructure/i18n/languages/en/home.translations.ts
+++ src/infrastructure/i18n/languages/en/home.translations.ts
@@ -8,11 +8,11 @@ export default {
   // Modal
   'component.home.modal.get.started.header': 'Get started',
   'component.home.modal.create.account.header': 'Create account',
-  'component.home.modal.login.via.google.button': 'Log in with google',
-  'component.home.modal.login.via.linkedin.button': 'Log in with linkedin',
-  'component.home.modal.login.via.github.button': 'Log in with github',
+  'component.home.modal.login.via.google.button': 'Login via Google',
+  'component.home.modal.login.via.linkedin.button': 'Login via LinkedIn',
+  'component.home.modal.login.via.github.button': 'Login via GitHub',
   'component.home.modal.privacy.and.policy': 'Privacy & Policy',
   'component.home.modal.terms.and.conditions': 'Terms & Conditions',
   'component.home.modal.info':
-    'By clicking Log in, you agree to our {{0}}. Learn how we process your data in our {{1}}.',
+    'By clicking Login, you agree to our {{0}}. Learn how we process your data in our {{1}}.',
 };
