44dad989b38281f9da6ac363ff54bcb30f243c0c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-08-23 18:41:07 +0200

    refactor: use camelcase function from package

--- src/utils/functions/string-array-to-lower-case/string-array-to-lower-case.function.ts
+++
@@ -1,9 +0,0 @@
-import { isStringArray } from '@utils';
-
-export const stringArrayToLowerCase = (array: string[]) => {
-  if (!isStringArray(array)) {
-    return array;
-  }
-
-  return array.map((_value) => _value.toLowerCase());
-};
