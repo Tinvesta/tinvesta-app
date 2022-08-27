b67fcf36ad9a40ca786979adaa6d6ebae72f46c8
playerony <pawel.wojtasinski.1995@gmail.com>
2022-08-27 22:01:46 +0200

    feat: display loader while upload images dynamic loading

--- src/ui/layout/center-block/center-block.styles.ts
+++ src/ui/layout/center-block/center-block.styles.ts
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.div<{ minHeight?: string }>`
   width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
+
+  ${({ minHeight }) => minHeight && `min-height: ${minHeight}`};
 `;
 
 const S = {
