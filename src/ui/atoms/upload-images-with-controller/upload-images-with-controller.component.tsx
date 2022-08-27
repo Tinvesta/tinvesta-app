bc89fb0671d2964e823a7387e77a579d41505798
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-08-27 22:13:08 +0200

    feat: display loader while upload images dynamic loading (#692)

--- src/ui/atoms/upload-images-with-controller/upload-images-with-controller.component.tsx
+++ src/ui/atoms/upload-images-with-controller/upload-images-with-controller.component.tsx
@@ -1,11 +1,21 @@
 import dynamic from 'next/dynamic';
 import { Controller } from 'react-hook-form';
 
+import { CenterBlockLayout, Loader } from '@ui';
+
 import { IUploadImagesProps } from './parts';
 import { IUploadImagesWithControllerProps } from './upload-images-with-controller.types';
 
-const UploadImages = dynamic<IUploadImagesProps>(() =>
-  import('./parts/upload-images/upload-images.component').then((_module) => _module.UploadImages),
+const UploadImages = dynamic<IUploadImagesProps>(
+  () =>
+    import('./parts/upload-images/upload-images.component').then((_module) => _module.UploadImages),
+  {
+    loading: () => (
+      <CenterBlockLayout minHeight="250px">
+        <Loader size="small" />
+      </CenterBlockLayout>
+    ),
+  },
 );
 
 export const UploadImagesWithController = <TFieldValues,>({
